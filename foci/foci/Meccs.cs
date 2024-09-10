95ae3339d3d0e48dd289f90deccdada71efd6bb5
KBencee <kissbence225@gmail.com>
2024-09-10 12:45:30 +0200

    alapok

---
+++ foci/foci/Meccs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foci
+{
+    internal class Meccs
+    {
+        public int fordulo {  get; set; }
+        public int hazaiGol { get; set; }
+        public int vendegGol { get; set; }
+        public int felHazai {  get; set; }
+        public int felVendeg { get; set; }
+        public string hazaiNev { get; set; }
+        public string vendegNev { get; set; }
+    }
+}
